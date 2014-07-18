65f5eb594057def63254cf241e552ac44ee10bb4
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-07-18 14:30:18 -0400

    owl controller js files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
     console.log('loaded bro');
 
+    $("#owl-demo").owlCarousel({
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+  });
+
     $('#twitter-icon').mouseover(function(event) {
         changeHeadshot(0);
         changeIcon('twitter-icon', 'images/icons/twitter_color.png');
