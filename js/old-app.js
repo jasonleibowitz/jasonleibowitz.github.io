5bf6d4e258ee63731de66ba2620a7fa734cf694d
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-10-10 01:26:15 -0400

    the site is now totally responsive with media queries and only has one html, one css and one js file for style

---
+++ js/old-app.js
@@ -0,0 +1,143 @@
+$(document).ready(function () {
+    console.log('loaded bro');
+
+    if(WURFL.is_mobile){
+      console.log("mobile device");
+      $('#stylesheet').remove();
+      $('#mixitup').remove();
+      $('head').append("<link rel='stylesheet' href='css/mobile-stylesheet.css'>");
+    } else {
+      console.log('desktop device');
+    }
+
+    setIntervalArrowAnimation();
+
+    // $('#Container').mixItUp();
+
+    $('#twitter-icon').mouseover(function(event) {
+        changeHeadshot(0);
+        changeIcon('twitter-icon', 'images/icons/twitter_color.png');
+    });
+    $('#twitter-icon').mouseleave(function(event){
+      defaultHeadshot();
+      changeIcon('twitter-icon', 'images/icons/twitter_default.png');
+    });
+
+    $('#instagram-icon').mouseover(function(event) {
+        changeHeadshot(-770);
+        changeIcon('instagram-icon', 'images/icons/instagram_color.png');
+    });
+    $('#instagram-icon').mouseleave(function(event){
+      defaultHeadshot();
+      changeIcon('instagram-icon', 'images/icons/instagram_default.png');
+    });
+
+    $('#linkedin-icon').mouseover(function(event) {
+        changeHeadshot(-180);
+        changeIcon('linkedin-icon','images/icons/linkedin_color.png');
+    });
+    $('#linkedin-icon').mouseleave(function(event){
+      defaultHeadshot();
+      changeIcon('linkedin-icon','images/icons/linkedin_default.png');
+    });
+
+    $('#github-icon').mouseover(function(event) {
+        changeHeadshot(-580);
+        changeIcon('github-icon','images/icons/github_color.png');
+    });
+    $('#github-icon').mouseleave(function(event){
+      defaultHeadshot();
+      changeIcon('github-icon','images/icons/github_default.png');
+    });
+
+    $('#blog-icon').mouseover(function(event) {
+      changeHeadshot(-990);
+    });
+    $('#blog-icon').mouseleave(function(event){
+      defaultHeadshot();
+    });
+
+    $.localScroll();
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+
+    $(window).resize(function(){
+       console.log('resize called');
+       var width = $(window).width();
+       if(width >= 320 && width <= 568){
+           $('.fa-twitter').removeClass('fa-4x').addClass('fa-2x');
+           $('.twitter').removeClass('col-md-1').addClass('col-xs-1');
+           $('.twitter').removeClass('col-md-offset-4');
+           $('.fa-instagram').removeClass('fa-4x').addClass('fa-2x');
+           $('.instagram').removeClass('col-md-1').addClass('col-xs-1');
+           $('.fa-linkedin-square').removeClass('fa-4x').addClass('fa-2x');
+           $('.linkedin').removeClass('col-md-1').addClass('col-xs-1');
+           $('.fa-github').removeClass('fa-4x').addClass('fa-2x');
+           $('.github').removeClass('col-md-1').addClass('col-xs-1');
+           $('.fa-envelope-o').removeClass('fa-4x').addClass('fa-2x');
+           $('.fa-linkedin').removeClass('fa-4x').addClass('fa-2x');
+           $('.fa-file-text-o').removeClass('fa-4x').addClass('fa-2x');
+
+       }
+    })
+    .resize();//trigger the resize event on page load.
+
+});
+
+$(window).load(function(){
+  setPortfolioSectionHeight();
+})
+
+
+
+});
+
+function defaultHeadshot(){
+  $('img.headshot').css('margin-left', -385);
+}
+
+function changeHeadshot(position){
+  $('img.headshot').css('margin-left', position);
+}
+
+function changeIcon(icon, img){
+  $('#'+icon).attr('src', img);
+}
+
+function setIntervalArrowAnimation() {
+  var intervalOnID = setInterval(animateArrow, 1000);
+  var intervalOffID = setInterval(animateOffArrow, 4000);
+}
+
+function animateArrow(){
+  $('.fa-hand-o-up').addClass('animated bounce');
+}
+
+function animateOffArrow(){
+  $('.fa-hand-o-up').removeClass('animated bounce');
+}
+
+function setPortfolioSectionHeight(){
+  var height = 0;
+  $("#portfolio > *").each(function(){
+    height += $(this).height();
+  });
+  console.log(height);
+  $("#portfolio").css('height', height + 100 + "px");
+}
+
+var changesForDesktop = function (){
+  // Resize social media icons
+  $("#twitter").removeClass('fa-3x').addClass('fa-4x')
+  $("#instagram").removeClass('fa-3x').addClass('fa-4x')
+  $("#linkedin").removeClass('fa-3x').addClass('fa-4x')
+  $("#github").removeClass('fa-3x').addClass('fa-4x')
+  $("#rss").removeClass('fa-3x').addClass('fa-4x')
+
+
+}
