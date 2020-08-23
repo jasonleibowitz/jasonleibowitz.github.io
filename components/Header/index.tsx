51c0e486be05e0579c83501cc10f6b84448d5ca6
Jason Leibowitz <jason@leibowitz.me>
2020-08-23 11:21:03 -0400

    Create NavLink that highlights the active page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { NavLink } from './NavLink';
+
 import styles from './styles.module.scss';
 
 export default function Header() {
@@ -12,11 +14,10 @@ export default function Header() {
         </a>
       </Link>
       <div className={styles.links}>
-        <Link href="/"><a>Home</a></Link>
-        <Link href="/about"><a>About</a></Link>
-        <Link href="/lists"><a>Lists</a></Link>
-        <Link href="/photos"><a>Photos</a></Link>
-        <Link href="/posts"><a>Blog</a></Link>
+        <NavLink href="/about">About</NavLink>
+        <NavLink href="/lists">Lists</NavLink>
+        <NavLink href="/photos">Photos</NavLink>
+        <NavLink href="/posts">Posts</NavLink>
       </div>
     </div>
   );
